98a5554aa9ee0728770a6915052532c3c5947875
Jacen <jacenlin87@gmail.com>
2019-08-01 20:28:33 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Jacen Lin on 1/8/19.
+//  Copyright © 2019 Jacen Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
